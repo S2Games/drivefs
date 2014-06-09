1d07d82b87ecd96b7e15675e5742af3010dac0b0
eliothedeman <eliot.d.hedeman@gmail.com>
2014-06-09 19:52:16 -0400

    initial test commit

---
+++ test.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"code.google.com/p/goauth2/oauth"
+	"io"
+	"os"
+	//drive "code.google.com/p/google-api-go-client/drive/v2"
+	"fmt"
+	"log"
+)
+
+func main() {
+
+	clientId := "266884823682-qfd1cvgpe2vucokeuhv0qr0bihn87ge4.apps.googleusercontent.com"
+	clientSecret := "6SGgwuVL8KMyFsigaPn2MGBK"
+	scope := "https://www.googleapis.com/auth/buzz"
+	redirectURL := "urn:ietf:wg:oauth:2.0:oob"
+	authURL := "https://accounts.google.com/o/oauth2/auth"
+	tokenURL := "https://accounts.google.com/o/oauth2/token"
+	requestURL := "https://www.googleapis.com/drive/v2/files"
+	code := "4/Dy_oHh2NXFotmPD0rRTBdThTHX41.gkgjYQdAoFQQmmS0T3UFEsOAEq0FjQI"
+	cachefile := "cache.json"
+
+	// Set up a configuration.
+	config := &oauth.Config{
+		ClientId:     clientId,
+		ClientSecret: clientSecret,
+		RedirectURL:  redirectURL,
+		Scope:        scope,
+		AuthURL:      authURL,
+		TokenURL:     tokenURL,
+		TokenCache:   oauth.CacheFile(cachefile),
+	}
+	transport := &oauth.Transport{Config: config}
+	token, err := config.TokenCache.Token()
+	if err != nil {
+		if code == "" {
+			// Get an authorization code from the data provider.
+			// ("Please ask the user if I can access this resource.")
+			url := config.AuthCodeURL("")
+			fmt.Println("Visit this URL to get a code, then run again with -code=YOUR_CODE\n")
+			fmt.Println(url)
+			return
+		}
+		// Exchange the authorization code for an access token.
+		// ("Here's the code you gave the user, now give me a token!")
+		token, err = transport.Exchange(code)
+		if err != nil {
+			log.Fatal("Exchange:", err)
+		}
+		// (The Exchange method will automatically cache the token.)
+		fmt.Printf("Token is cached in %v\n", config.TokenCache)
+	}
+	transport.Token = token
+	r, err := transport.Client().Get(requestURL)
+	if err != nil {
+		log.Fatal("Get", err)
+	}
+	defer r.Body.Close()
+	io.Copy(os.Stdout, r.Body)
+	fmt.Println()
+}
